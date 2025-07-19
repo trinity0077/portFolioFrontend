43e4e410ad4136d70acc67dded5735cc98efa7fe
Camille <gryspeerdt.camille@gmail.com>
2025-07-19 21:59:31 +0200

    ajout du projet my moviez a la liste des redirections

--- components/FolioNavAndRedirection.tsx
+++ components/FolioNavAndRedirection.tsx
@@ -47,7 +47,9 @@ const FolioNavAndRedirection = () => {
       </Head>
       <nav className={styles.nav}>
         <button
-          className={`${styles.button} ${activeButton === 1 ? styles["button-active"] : ""}`}
+          className={`${styles.button} ${
+            activeButton === 1 ? styles["button-active"] : ""
+          }`}
           onClick={() => handleButtonClick(1)}
         >
           It's Me
@@ -55,51 +57,61 @@ const FolioNavAndRedirection = () => {
 
         {/* Projet avec menu déroulant */}
         <div className={styles.dropdown}>
+          <button
+            className={`${styles.button} ${styles["projects-button"]} ${
+              showProjectsMenu || activeButton === 2
+                ? styles["button-active"]
+                : ""
+            }`}
+            onClick={() => setShowProjectsMenu((prev) => !prev)}
+          >
+            Projets
+          </button>
+          {showProjectsMenu && (
+            <div
+              className={styles.dropdownMenu}
+              onMouseLeave={() => setShowProjectsMenu(false)}
+            >
+              <button
+                onClick={() => {
+                  handleButtonClick(2);
+                  setShowProjectsMenu(false); // fermer le menu au clic
+                }}
+              >
+                CigFree
+              </button>
+              <button
+                onClick={() => {
+                  handleOpenProject("https://newspaper-front-two.vercel.app");
+                  setShowProjectsMenu(false);
+                }}
+              >
+                Morning News
+              </button>
+              <button
+                onClick={() => {
+                  handleOpenProject(
+                    "https://mymoviez-frontend-nine.vercel.app/"
+                  );
+                  setShowProjectsMenu(false);
+                }}
+              >
+                My Moviez
+              </button>
+            </div>
+          )}
+        </div>
+
         <button
-  className={`${styles.button} ${styles["projects-button"]} ${
-    showProjectsMenu || activeButton === 2 ? styles["button-active"] : ""
-  }`}
-  onClick={() => setShowProjectsMenu((prev) => !prev)}
->
-  Projets
-</button>
-{showProjectsMenu && (
-  <div
-    className={styles.dropdownMenu}
-    onMouseLeave={() => setShowProjectsMenu(false)}
-  >
-<button
-  onClick={() => {
-    handleButtonClick(2);
-    setShowProjectsMenu(false); // fermer le menu au clic
-  }}
->
-  CigFree
-</button>
-  <button onClick={() => {
-    handleOpenProject("https://newspaper-front-two.vercel.app");
-    setShowProjectsMenu(false);}}>
-      Morning News
-  </button>
-  <button onClick={() => {
-    handleOpenProject("https://mymoviez-frontend-nine.vercel.app/");
-    setShowProjectsMenu(false);}}>
-      My Moviez
-  </button>
-
-  </div>
-)}
-
-
-
- </div>
-
-        <button
-          className={`${styles["container-email"]} ${activeButton === 3 ? styles["Active-container-email"] : ""}`}
+          className={`${styles["container-email"]} ${
+            activeButton === 3 ? styles["Active-container-email"] : ""
+          }`}
           onClick={() => handleButtonClick(3)}
         >
           <img
-            className={`${styles["emailIconPlane"]} ${activeButton === 3 ? styles["Active-emailIconPlane"] : ""}`}
+            className={`${styles["emailIconPlane"]} ${
+              activeButton === 3 ? styles["Active-emailIconPlane"] : ""
+            }`}
             src="/paper-plane.svg"
             alt="Icon"
             style={{ width: "50px", height: "50px" }}
@@ -108,11 +120,28 @@ const FolioNavAndRedirection = () => {
         </button>
       </nav>
 
-      <div >
+      <div>
         {activeButton === 1 && (
-          <div style={{ padding: "2rem", backgroundColor: "#ffffffff",alignItems: "center" }}>
-            <TypingAnimation className={styles.typingText} duration={110}>Camille Gryspeerdt</TypingAnimation>
-            <TypingAnimation className={styles.typingTextP} duration={45} delay= {1900} as="h2">Rigoureux, dynamique et passionné par la recherche de solutions innovantes pour améliorer la qualité des produits, je suis prêt à mettre mes compétences en pratique dans de nouveaux projets.</TypingAnimation>
+          <div
+            style={{
+              padding: "2rem",
+              backgroundColor: "#ffffffff",
+              alignItems: "center",
+            }}
+          >
+            <TypingAnimation className={styles.typingText} duration={110}>
+              Camille Gryspeerdt
+            </TypingAnimation>
+            <TypingAnimation
+              className={styles.typingTextP}
+              duration={45}
+              delay={1900}
+              as="h2"
+            >
+              Rigoureux, dynamique et passionné par la recherche de solutions
+              innovantes pour améliorer la qualité des produits, je suis prêt à
+              mettre mes compétences en pratique dans de nouveaux projets.
+            </TypingAnimation>
           </div>
         )}
 
@@ -128,7 +157,6 @@ const FolioNavAndRedirection = () => {
           </div>
         )}
       </div>
-      
     </div>
   );
 };
